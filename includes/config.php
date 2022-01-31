d1dd84f799a0520b0d7ddb82e5a2c930981173e0
pocusnik <maksim.zharow58@gmail.com>
2022-01-31 11:07:19 +0300

    first commit

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('SITE_NAME', 'Twitter');
+define('HOST', 'http://'. $_SERVER['HTTP_HOST']);
+define('DB_HOST', $_SERVER['HTTP_HOST']);
+define('DB_NAME', 'twitter');
+define('DB_USER', 'twitter');
+define('DB_PASS', '123123');
+?>
---
+++ user_posts.php
@@ -0,0 +1,9 @@
+<?php
+include_once "includes/functions.php";
+$posts = get_posts($user_id);
+include_once "includes/header.php";
+include_once "includes/tweet_form.php";
+include_once "includes/posts.php";
+include_once "includes/footer.php";
+?>
+		
